687ff4d30af41b72296c08f2687c6f9c986f54e0
Youdo <notneedtoknowy@gmail.com>
2025-02-05 22:54:14 +0300

    added saving button for requests (as txt)

--- gui/windows/requests.py
+++ gui/windows/requests.py
@@ -6,8 +6,9 @@
 
 import os
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
 from manager.blanks import gather
+from gui.src import show_warning_messagebox
 
 class RequestsList(QDialog):
     """
@@ -25,6 +26,7 @@ class RequestsList(QDialog):
         self.setModal(True)
         # обработка кнопок
         self.ui.button_ok.clicked.connect(self.close)
+        self.ui.button_save.clicked.connect(self.save_requests)
         # заполнение параметров
         self.fill_requests()
 
@@ -38,3 +40,19 @@ class RequestsList(QDialog):
             for req in item[2]:
                 text += gather(req[0])
         self.text_commands.appendPlainText(text)
+
+    def save_requests(self) -> None:
+        # открытие окна сохранения файла
+        filepath, _ = QFileDialog.getSaveFileName()
+        if False == filepath.endswith(".txt"):
+            filepath = filepath + ".txt"
+        # сохранение содержимого запроса
+        request = ""
+        for item in self.parent.exp_list:
+            request += f"Тикет:{item[0]}, задачи:{item[3]}\n"
+            for req in item[2]:
+                request += gather(req[0])
+        with open (filepath, "w") as f:
+            f.write(request)
+            f.close()
+        show_warning_messagebox(f'Выгружено в файл {filepath}')
