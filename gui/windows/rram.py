f23fd13aebe1a83d6ab616014644fc28685f6070
Youdo <notneedtoknowy@gmail.com>
2025-03-22 16:31:24 +0300

    rram window opening

---
+++ gui/windows/rram.py
@@ -0,0 +1,30 @@
+"""
+Окно информации о кроссбаре
+"""
+
+import os
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog
+
+class Rram(QDialog):
+    """
+    Информация о кроссбаре
+    """
+
+    GUI_PATH = os.path.join("gui","uies","rram.ui")
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.parent = parent
+        # загрузка ui
+        self.ui = uic.loadUi(self.GUI_PATH, self)
+        # доп настройки
+        self.setModal(True)
+        # значения по умолчанию
+        self.set_up_init_values()
+
+    def set_up_init_values(self) -> None:
+        """
+        Установка значений по умолчанию
+        """
+        self.ui.button_interrupt.setEnabled(False)
