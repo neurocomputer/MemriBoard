6cc10813adae175ddaf604791a4729350f0f4705
Youdo <notneedtoknowy@gmail.com>
2025-04-18 15:19:52 +0300

    updated data from other branch

--- gui/windows/rram.py
+++ gui/windows/rram.py
@@ -3,11 +3,11 @@
 """
 
 import os
+import shutil
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog, QFileDialog
 from PyQt5.QtGui import QPixmap
 from copy import deepcopy
-
 from gui.src import show_warning_messagebox
 
 class Rram(QDialog):
@@ -29,6 +29,7 @@ class Rram(QDialog):
         # обработчики кнопок
         self.ui.button_apply_tresh.clicked.connect(self.apply_tresh)
         self.ui.button_read.clicked.connect(self.parent.read_cell_all)
+        self.ui.button_save_img.clicked.connect(self.save_heatmap)
         self.ui.button_save.clicked.connect(self.save_text)
         self.ui.button_load.clicked.connect(self.load_text)
 
@@ -56,6 +57,16 @@ class Rram(QDialog):
                     rram_data[i][j] = 0
         self.parent._snapshot(mode="rram", data=rram_data)
         self.ui.label_rram_img.setPixmap(QPixmap(os.path.join("gui","uies","rram.png")))
+
+    def save_heatmap(self) -> None:
+        """
+        Сохранение снимка
+        """
+        save_path = QFileDialog.getExistingDirectory(self, "Выберите директорию для сохранения")
+        save_path = os.path.join(save_path, "heatmap.png")
+        picture_path = os.path.join("gui","uies","rram.png")
+        shutil.copy(picture_path, save_path)
+        show_warning_messagebox('Снимок сохранен в ' + save_path)
 
     def save_text(self) -> None:
         """
