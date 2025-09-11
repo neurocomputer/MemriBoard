7fb031e7ace8d09b38b2b7344ab247af2c80a0b3
QAZYAE <nike200358@gmail.com>
2025-09-11 13:48:45 +0300

    Hacky fix for tests with many points

--- gui/windows/testing.py
+++ gui/windows/testing.py
@@ -363,6 +363,9 @@ class Testing(QWidget):
             #                       ])
         self.csv_names.append(fname+'\n')
         # рисунок для базы в matplotlib
+        plt.rcParams['agg.path.chunksize'] = 20000  # FIXME: This is a hacky fix
+        # При исполнении теста на множество ячеек с большим количеством точек (endurance 1e5 циклов)
+        # рисунок не сохраняется (OverflowError). Возможно, лучше рисовать не все точки
         plt.clf()
         if len(data_for_plot_x) > 1000:
             plt.plot(data_for_plot_x[0:1000], data_for_plot_y[0:1000], marker='o', linewidth=0.5)
