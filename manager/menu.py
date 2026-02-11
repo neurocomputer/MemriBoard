a94bae98d2753d3dd464cb984900987aa3d17e01
QAZYAE <nike200358@gmail.com>
2026-02-11 16:59:07 +0300

    New task generator

--- manager/menu.py
+++ manager/menu.py
@@ -1,9 +1,10 @@
 """
 Меню режимов
 """
-from manager.modes import get_tst, get_std
+from manager.modes import get_tst, get_std, get_smu_iv_dc
 
 menu: dict = {
         'tst': get_tst,
-        'std': get_std
+        'std': get_std,
+        'smu_iv_dc': get_smu_iv_dc
     }
---
+++ manager/modes/smu.py
@@ -0,0 +1,126 @@
+"""
+Генераторы тасков для измерения при помощи SMU на VISA-инструментах. 
+Создается три типа тасков:
+1. Конфигурационный (config) - для конфигурации эксперимента на оборудовании, 
+2. Таск чтения (sense) - для считывания текущего значения 
+тока в процессе измерения,
+3. Таск окончания тикета (ticket_end) для сброса оборудования. 
+Тип таска указывается в поле mode_flag.
+
+Типовик запроса:
+params =  {
+    "v_dir_strt_inc": 0,
+    "v_dir_stop_inc": 819,
+    "v_dir_step_inc": 410,
+    "t_dir_msec_inc": 0,
+    "t_dir_usec_inc": 100,
+    "dir_inc_countr": 1,
+    
+    "v_dir_strt_dec": 819,
+    "v_dir_stop_dec": 0,
+    "v_dir_step_dec": 410,
+    "t_dir_msec_dec": 0,
+    "t_dir_usec_dec": 100,
+    "dir_dec_countr": 1,
+    
+    "v_rev_strt_inc": 0,
+    "v_rev_stop_inc": 819,
+    "v_rev_step_inc": 410,
+    "t_rev_msec_inc": 0,
+    "t_rev_usec_inc": 100,
+    "rev_inc_countr": 1,
+    
+    "v_rev_strt_dec": 819,
+    "v_rev_stop_dec": 0,
+    "v_rev_step_dec": 410,
+    "t_rev_msec_dec": 0,
+    "t_rev_usec_dec": 100,
+    "rev_dec_countr": 1,
+    
+    "count": 1,
+    "reverse": 0,
+    
+    "id": 0,
+    "wl": 0,
+    "bl": 0,
+    
+    "dir_soft_cc": 0.0003,
+    "rev_soft_cc": 0.1
+}
+"""
+
+import numpy as np
+from typing import Generator
+from manager.terminate import terminators
+
+def get_smu_iv_dc(
+    params: dict, 
+    terminate: dict, 
+    blank_type: str
+) -> Generator[list, None, None]:
+    """Генератор для режима SMU_IV_DC.
+
+    Args:
+        params (dict): Experiment params.
+        terminate (dict): Terminator type and value.
+        blank_type (str): Blank type (blanks.py).
+
+    Yields:
+        Generator[list]: _description_
+    """
+    modes = {'dir': 0,
+             'rev': 1}
+    terminator = terminators[terminate['type']](terminate['value'])
+    n_points = {}
+    v_arrays = {}
+    double = {}
+    for dir in ['dir', 'rev']:
+        try:
+            v_arrays[dir] = np.arange(
+                params[f'v_{dir}_strt_inc'],
+                params[f'v_{dir}_stop_inc'] + params[f'v_{dir}_step_inc'],
+                params[f'v_{dir}_step_inc']
+            )
+            n_points[dir] = len(v_arrays[dir])
+            double[dir] = True if params[f'v_{dir}_strt_dec'] != 0 else False
+        except ZeroDivisionError:
+            n_points[dir] = 0
+    for _ in range(params['count']):
+        # порядок dir-rev
+        sequence = ['rev', 'dir'] if params['reverse'] else ['dir', 'rev']
+        for dir in sequence:
+            if n_points[dir] != 0:
+                config_data = {'mode_flag': 'config_iv_dc',
+                               'vol': 0,
+                               't_ms': params[f't_{dir}_msec_inc'],
+                               't_us': params[f't_{dir}_usec_inc'],
+                               'id': params['id'],
+                               'sign': modes[dir],
+                               'v_start': v_arrays[dir][0],
+                               'v_stop': v_arrays[dir][-1],
+                               'n_points': n_points[dir],
+                               'double': double[dir]}
+                if 'wl' in params and 'bl' in params:
+                    config_data['wl'] = params['wl']
+                    config_data['bl'] = params['bl']
+                else:
+                    config_data['wl'] = 0
+                    config_data['bl'] = 0
+                yield [config_data, terminator]  # Config task
+                sense_data = {'mode_flag': 'sense',
+                              'vol': 0,
+                              't_ms': params[f't_{dir}_msec_inc'],
+                              't_us': params[f't_{dir}_usec_inc'],
+                              'id': params['id'],
+                              'sign': modes[dir]}
+                for _ in range(params[f'{dir}_inc_countr']):
+                    for vol in v_arrays[dir]:
+                        sense_data['vol'] = abs(int(vol))
+                        yield [sense_data, terminator]  # Sense task
+                    if double[dir]:
+                        for vol in v_arrays[dir][::-1]:
+                            sense_data['vol'] = abs(int(vol))
+                            yield [sense_data, terminator]  # Sense task
+    sense_data['mode_flag'] = 'ticket_end'
+    sense_data['vol'] = 0
+    yield [sense_data, terminator]  # Ticket_end task
