73b688595665647451e23f94baa50c155db716d1
QAZYAE <nike200358@gmail.com>
2026-02-12 14:44:53 +0300

    pause_experiment blocked for VISA

--- gui/windows/apply.py
+++ gui/windows/apply.py
@@ -52,6 +52,11 @@ class Apply(QWidget):
         """
         super().__init__(parent)
         self.parent = parent
+        # Флаг для VISA-инструментов
+        if self.parent.man.conn.board_type in ['VISA',]:
+            self.on_VISA = True
+        else:
+            self.on_VISA = False
         # загрузка ui
         self.ui = uic.loadUi(self.GUI_PATH, self)
         self.ui.change_language()
@@ -278,6 +283,8 @@ class Apply(QWidget):
         """
         Блокировка кнопок
         """
+        if self.on_VISA:
+            flags[2] = True  # Блокировка кнопки Пауза для VISA-инструментов
         self.ui.button_start.setDisabled(flags[0])
         self.ui.button_graph_settings.setDisabled(flags[1])
         self.ui.button_pause.setDisabled(True)
@@ -468,10 +475,6 @@ class ApplyExp(QThread):
         """
         Запуск потока посылки тикета
         """
-        if self.parent.parent.man.conn.board_type in ['VISA',]:
-            on_VISA = True
-        else:
-            on_VISA = False  # Flag for VISA instruments
         for item in self.parent.coordinates:
             # todo: подобный функционал должен быть в manager
             if self.parent.parent.man.ap_config['board']['cc_type'] == 'soft':
@@ -629,10 +632,7 @@ class ApplyExp(QThread):
                     self.count_changed.emit(task_counter)
                 #print("Весь цикл:", time.time()-start_time_loop)
                 # закрываем файл результата
-                result_file.close()
-                # Пытаемся остановить эксперимент на VISA-устройстве 
-                if on_VISA and self.need_stop:
-                    self.parent.parent.man.conn.impact({'mode_flag': 'panic', 'wl': item[0], 'bl': item[1], 'id': 0})     
+                result_file.close()  
                 # сохраняем в БД статус завершения
                 if result:
                     last_resistance = int(resistance_previous)  # Если результат есть, то сопротивление уже посчитали на последнем шаге цикла
@@ -682,7 +682,5 @@ class ApplyExp(QThread):
             if self.need_stop:
                 break
             #time.sleep(self.PAUSE_TIME*3) # ожидание между мемристорами чтобы успело сохранить в БД
-        if on_VISA:  # Отключаем все ячейки в кроссбаре от источника
-            self.parent.parent.man.conn.impact({'mode_flag': 'standby', 'wl': item[0], 'bl': item[1], 'id': 0})          
         self.finished_exp.emit(f'{stop_reason},{self.flag_soft_cc}') # успешно завершен
         #time.sleep(self.PAUSE_TIME)
