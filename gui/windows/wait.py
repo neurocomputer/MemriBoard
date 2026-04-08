c0ab6aa2a6fe5adf14ae5051bb16de12f93de365
QAZYAE <nike200358@gmail.com>
2026-04-08 22:08:51 +0300

    Stop resistance scanning on Wait window closing

--- gui/windows/wait.py
+++ gui/windows/wait.py
@@ -7,6 +7,7 @@
 import os
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSignal
 
 class Wait(QDialog):
     """
@@ -15,6 +16,7 @@ class Wait(QDialog):
 
     GUI_PATH = os.path.join("gui","uies","wait.ui")
     history: list
+    stop_experiment = pyqtSignal()
 
     def __init__(self, opener=None, parent=None) -> None:
         super().__init__(parent)
@@ -26,6 +28,7 @@ class Wait(QDialog):
         self.setModal(True)
 
     def closeEvent(self, event) -> None:
+        self.stop_experiment.emit()
         if self.opener == 'new_ann':
             self.parent.new_ann_dialog.fill_table_weights()
             event.accept()
